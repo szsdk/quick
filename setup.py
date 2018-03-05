8b675c65d79c87327eaa3033c8aac18677b1435a
szsdk <shenz34206@hotmail.com>
2018-03-05 22:03:07 +0800

    add setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(name='guick',
+      version='1.0',
+      description='Draw qt gui for click script',
+      author='Shen Zhou',
+      author_email='shenz34206@hotmail.com',
+      py_modules=['guick'],
+      install_requires=[
+          'click>=5.0',
+      ]
+      )
