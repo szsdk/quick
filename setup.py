835f1bda1424323bbca73e90405af20bc4d6717c
Keith Gray <keith@thegrays.net>
2020-02-04 12:57:38 -0600

    Added PyQt5 as a setup.py dependency

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(name='quick',
       py_modules=['quick'],
       install_requires=[
           'click>=5.0',
+          'PyQt5'
           ],
       extras_require={
           'qtstyle':  ["qdarkstyle"]
